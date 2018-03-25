f304b10aef3be6e8bcb4cb76d84ee2128906decb
Sarah Dawson <knittingarch@gmail.com>
2018-03-25 13:24:08 -0400

    Attempt to Use react-form To Create Easy Forms
    Thsi commit uses react-form.  There are still some bugs to work out as it doesn't not work out of the box with email, password, or telephone fields.  Instead you have to use regular HTML (I think ) in order to create those components.  There was also the suggestion inthe react-form Slack to try FAAC for a better understanding of how <Field /> works.

--- sweepthis/src/App.js
+++ sweepthis/src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 import JoinButton from './Buttons/JoinButton';
-import JoinForm from './Forms/JoinForm';
 import SignInButton from './Buttons/SignInButton';
-import SignInForm from './Forms/SignInForm';
 import './App.css';
 import './Buttons/JoinButton.css';
-import './Forms/JoinForm.css';
 import './Buttons/SignInButton.css';
-import './Forms/SignInForm.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>This is where the text will go!</p>
-        <SignInButton />
         <JoinButton />
-        <JoinForm />
-        <SignInForm />
+        <SignInButton />
       </div>
     );
   }
--- sweepthis/src/Forms/SignInForm.js
+++ sweepthis/src/Forms/SignInForm.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Form, Text } from 'react-form';
 
 const signInForm = () => {
     return (
-        <form>
-            <input type="email" name="email" id="email" placeholder="Email"/>
-            <input type="password" name="password1" id="password1" placeholder="Password"/>
-            <input type="submit" value="Sign In" />
-        </form>
-    )
-};
+        <Form onSubmit={submittedValues => this.setState({ submittedValues })}>
+            {formApi => (
+                <form onSubmit={ formApi.submitForm } id="signinform">
+                    <Text field="email" placeholder="Email" />
+                    <button type="submit">Sign In</button>
+                </form>
+            )}
+        </Form>
+    );
+}
+   
+
+// class SignInForm extends Component {
+//     render() {
+//        return (
+//         <Form onSubmit={submittedValues => this.setState({ submittedValues })}>
+//             {formApi => (
+//                 <form onSubmit={ formApi.submitForm } id="signinform">
+//                     <Text field="email" placeholder="Email" />
+//                     <button type="submit">Sign In</button>
+//                 </form>
+//             )}
+//         </Form>
+//         );
+//     }
+// }
+
+    
+
+//     return (
+//         <form>
+//             <Email field="email" placeholder="Email" />
+//             <Password field="password" placeholder="Password" />
+//             <input type="email" name="email" id="email" />
+//             <input type="password" name="password1" id="password1" placeholder="Password"/>
+//             <input type="submit" value="Sign In" />
+//         </form>
+//     )
+// };
 
 export default signInForm;
+// export default SignInForm;
