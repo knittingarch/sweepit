b243c35460fe788bb7859241546f461d6442ce76
Sarah Dawson <knittingarch@gmail.com>
2018-03-26 15:51:09 -0400

    Strip Out Dropdown Code

--- sweepthis/src/Street.js
+++ sweepthis/src/Street.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 const Street = (props) => {
     return (
-        <div className="dropdown">
-            <button className="dropbtn">Dropdown</button>
-            <div className="dropdown-content">
-                <a href="#">{props.details.street_name} {props.details.suffix}</a>
-            </div>
+        <div>
+            <a href="#">{props.details.street_name} {props.details.suffix}</a>
         </div>
     )
 };
